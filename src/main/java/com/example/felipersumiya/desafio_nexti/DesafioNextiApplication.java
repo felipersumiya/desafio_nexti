f9c7bf157913060c66abb74bc587596e3ecf7921
Felipe Roberto Sumiya <felipersumiya@gmail.com>
2021-10-26 15:31:18 -0300

    Criação do projeto.

---
+++ src/main/java/com/example/felipersumiya/desafio_nexti/DesafioNextiApplication.java
@@ -0,0 +1,13 @@
+package com.example.felipersumiya.desafio_nexti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNextiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNextiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/felipersumiya/desafio_nexti/DesafioNextiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.felipersumiya.desafio_nexti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNextiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
